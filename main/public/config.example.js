d33db405eddbd91f9363c2435eca237a9aa65f53
xmunder <gomezcristian676@gmail.com>
2026-05-21 11:05:37 -0500

    chore(web): remove legacy scripts and add config template

---
+++ main/public/config.example.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = Object.freeze({
+  apiKey: 'REPLACE_ME',
+  authDomain: 'REPLACE_ME.firebaseapp.com',
+  projectId: 'REPLACE_ME',
+  storageBucket: 'REPLACE_ME.appspot.com',
+  messagingSenderId: 'REPLACE_ME',
+  appId: 'REPLACE_ME',
+  databaseURL: 'https://REPLACE_ME-default-rtdb.firebaseio.com',
+  measurementId: 'REPLACE_ME'
+});
